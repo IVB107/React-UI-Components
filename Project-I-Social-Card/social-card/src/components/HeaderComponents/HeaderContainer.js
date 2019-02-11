1932f4e9b78c18288ae8e48b02517bf6273c8cb7
IVB107 <ianvb107@gmail.com>
2019-02-11 16:11:29 -0600

    Completed Header Components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
 import './Header.css';
+
+const HeaderContainer = () => {
+    return (
+        <div className="container">
+            <ImageThumbnail />
+            <div className="header-text">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>   
+    )
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="title-container">
+            <p className="name"><strong>Lambda School</strong></p>
+            <p> @LambdaSchool</p>
+            <p> •</p>
+            <p> 26 jan</p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
