ebc00e53e6190c16cf6222bc4bf7072a76f38350
IVB107 <ianvb107@gmail.com>
2019-02-12 17:22:15 -0600

    updated text properties on action buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -36,29 +36,29 @@ class App extends React.Component {
         <CalculatorDisplay display={this.state.display}/>
         <div className="calculator-row">
           <ActionButton className="flex3 clear" text="clear" onClick={(props) => this.handleClearDisplay(props)}></ActionButton>
-          <ActionButton className="flex1">÷</ActionButton>
+          <ActionButton className="flex1" text="÷"></ActionButton>
         </div>
         <div className="calculator-row">
           <NumberButton text="7" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="8" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="9" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
-          <ActionButton className="flex1">x</ActionButton>
+          <ActionButton className="flex1" text="x"></ActionButton>
         </div>
         <div className="calculator-row">
           <NumberButton text="4" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="5" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="6" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
-          <ActionButton className="flex1">-</ActionButton>
+          <ActionButton className="flex1" text="-"></ActionButton>
         </div>
         <div className="calculator-row">
           <NumberButton text="1" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="2" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="3" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
-          <ActionButton className="flex1">+</ActionButton>
+          <ActionButton className="flex1" text="+"></ActionButton>
         </div>
         <div className="calculator-row">
           <ActionButton className="flex3" text="0" onClick={(props) => this.handleNumberClick(props)}></ActionButton>
-          <ActionButton className="flex1">=</ActionButton>
+          <ActionButton className="flex1" text="="></ActionButton>
         </div>
       </div>
     );
