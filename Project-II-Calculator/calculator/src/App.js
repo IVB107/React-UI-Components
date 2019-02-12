fd7ccba0c41cbd0ab122d6b2d146ea33d745d16d
IVB107 <ianvb107@gmail.com>
2019-02-12 17:07:31 -0600

    added click handler to display numbers

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,39 +4,61 @@ import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton'
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="calculator">
-      <CalculatorDisplay />
-      <div className="calculator-row">
-        <ActionButton className="flex3 clear">clear</ActionButton>
-        <ActionButton className="flex1">÷</ActionButton>
-      </div>
-      <div className="calculator-row">
-        <NumberButton>7</NumberButton>
-        <NumberButton>8</NumberButton>
-        <NumberButton>9</NumberButton>
-        <ActionButton className="flex1">x</ActionButton>
-      </div>
-      <div className="calculator-row">
-        <NumberButton>4</NumberButton>
-        <NumberButton>5</NumberButton>
-        <NumberButton>6</NumberButton>
-        <ActionButton className="flex1">-</ActionButton>
-      </div>
-      <div className="calculator-row">
-        <NumberButton>1</NumberButton>
-        <NumberButton>2</NumberButton>
-        <NumberButton>3</NumberButton>
-        <ActionButton className="flex1">+</ActionButton>
-      </div>
-      <div className="calculator-row">
-        <ActionButton className="flex3">0</ActionButton>
-        <ActionButton className="flex1">=</ActionButton>
-      </div>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+      display: 0
+    }
+  }
 
-  );
+  handleNumberClick = props => {
+    const newState = {...this.state, number: props};
+    if (newState.display === 0) {
+      this.setState({newState, display: newState.number});
+    } else {
+      this.setState({newState, display: `${this.state.display}${newState.number}`});
+    }
+  }
+
+  handleClearDisplay = props => {
+    console.log('something');
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <CalculatorDisplay display={this.state.display}/>
+        <div className="calculator-row">
+          <ActionButton className="flex3 clear" text="clear"></ActionButton>
+          <ActionButton className="flex1">÷</ActionButton>
+        </div>
+        <div className="calculator-row">
+          <NumberButton text="7" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <NumberButton text="8" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <NumberButton text="9" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <ActionButton className="flex1">x</ActionButton>
+        </div>
+        <div className="calculator-row">
+          <NumberButton text="4" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <NumberButton text="5" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <NumberButton text="6" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <ActionButton className="flex1">-</ActionButton>
+        </div>
+        <div className="calculator-row">
+          <NumberButton text="1" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <NumberButton text="2" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <NumberButton text="3" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
+          <ActionButton className="flex1">+</ActionButton>
+        </div>
+        <div className="calculator-row">
+          <ActionButton className="flex3" text="0" onClick={(props) => this.handleNumberClick(props)}></ActionButton>
+          <ActionButton className="flex1">=</ActionButton>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default App;
