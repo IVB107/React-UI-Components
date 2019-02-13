cda266c1f480676585abce676eecf275e57f446a
IVB107 <ianvb107@gmail.com>
2019-02-12 22:41:17 -0600

    Added divide event handler

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 const initialState = {
   number: 0,
+  evaluate: 0,
   display: '0'
 }
 class App extends React.Component {
@@ -13,13 +14,15 @@ class App extends React.Component {
     super(props);
     this.state = {
       number: 0,
+      evaluate: 0,
       display: '0'
     }
   }
 
   handleNumberClick = props => {
     const newState = {...this.state, number: props};
-    if (newState.display === '0') {
+    const maths = /[-*+/]/g;
+    if (newState.display === '0' || newState.display.match(maths)) {
       this.setState({newState, display: newState.number.toString()});
     } else {
       this.setState({newState, display: `${this.state.display}${newState.number}`});
@@ -30,31 +33,57 @@ class App extends React.Component {
     this.setState(initialState);
   }
 
+  handleDivide = props => {
+    // Having trouble getting this to work
+    const newState = {...this.state};
+    if (newState.evaluate === 0){
+      this.setState({newState, evaluate: `${newState.display}`, display: props});
+    } else {
+      this.setState({newState, evaluate: `${newState.evaluate}${props}`, display: props});
+    }
+  }
+
+  handleMultiply = props => {
+    console.log(props);
+  }
+
+  handleSubtract = props => {
+    console.log(props);
+  }
+
+  handleAdd = props => {
+    console.log(props);
+  }
+
+  handleEquals = props => {
+    console.log(props);
+  }
+
   render() {
     return (
       <div className="calculator">
         <CalculatorDisplay display={this.state.display}/>
         <div className="calculator-row">
           <ActionButton className="flex3 clear" text="clear" onClick={(props) => this.handleClearDisplay(props)}></ActionButton>
-          <ActionButton className="flex1" text="÷"></ActionButton>
+          <ActionButton className="flex1" text="÷" onClick={(props) => this.handleDivide('/')}></ActionButton>
         </div>
         <div className="calculator-row">
           <NumberButton text="7" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="8" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="9" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
-          <ActionButton className="flex1" text="x"></ActionButton>
+          <ActionButton className="flex1" text="x" onClick={(props) => this.handleMultiply(props)}></ActionButton>
         </div>
         <div className="calculator-row">
           <NumberButton text="4" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="5" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="6" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
-          <ActionButton className="flex1" text="-"></ActionButton>
+          <ActionButton className="flex1" text="-" onClick={(props) => this.handleSubtract(props)}></ActionButton>
         </div>
         <div className="calculator-row">
           <NumberButton text="1" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="2" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
           <NumberButton text="3" onClick={(props) => this.handleNumberClick(props)}></NumberButton>
-          <ActionButton className="flex1" text="+"></ActionButton>
+          <ActionButton className="flex1" text="+" onClick={(props) => this.handleAdd(props)}></ActionButton>
         </div>
         <div className="calculator-row">
           <ActionButton className="flex3" text="0" onClick={(props) => this.handleNumberClick(props)}></ActionButton>
